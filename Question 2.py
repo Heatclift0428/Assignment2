7f03eab8c99d305e2fbd93c482a6f31412683301
Heatclift0428 <apuradofrancis@gmail.com>
2022-04-25 20:41:11 +0800

    Selection Sort algorithm added

--- Question 2.py
+++ Question 2.py
@@ -1,19 +1,33 @@
 # This is Question 2 of Assignment 2
 # Francis Apurado
 
-
 import random
+
+# selection sort algorithm
+def SelectionSort(array, size):
+    for x in range(size):
+        minIndex = x
+
+        for y in range(x + 1, size):
+
+            if array[y] < array[minIndex]:  # will select the minimum for each loop #sorts in Ascending
+                minIndex = y
+
+        (array[x], array[minIndex]) = (array[minIndex], array[x])  # append the minimum in the right position
+
+
+
 
 while True:
     try:
-        UserChoice = float(input("1. Test an individual sorting algorithm \n "
+        UserChoice = float(input("1. Test an individual sorting algorithm\n"
                                  "2. Test Multiple sorting algorithms\n"))
 
-        if UserChoice == 1:
+        if UserChoice == 2:
             pass
             break
 
-        if UserChoice == 2:
+        if UserChoice == 1:
 
             AlgorithmChoice = float(input("1. selection sort \n"
                                           "2. insertion sort \n"
@@ -21,7 +35,7 @@ while True:
                                           "4. quick sort \n "
                                           "5. counting sort \n"))
 
-            arraySize = int(input("please enter the size of the array"))
+            arraySize = int(input("please enter the size of the array\n"))
 
             RandomArray = []
 
@@ -29,7 +43,10 @@ while True:
                 x = random.randint(0, 100)
                 RandomArray.append(x)
 
-            break
+            if AlgorithmChoice == 1:
+                print("Not sorted: ", RandomArray)
+                SelectionSort(RandomArray, len(RandomArray))
+                print('Sorted: ', RandomArray)
 
         else:
             print("please enter a value within the range")
