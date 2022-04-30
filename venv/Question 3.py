679f2bc72145b0ca7b1b74acab79beaf9f1efca2
Heatclift0428 <apuradofrancis@gmail.com>
2022-04-30 19:20:07 +0800

    Added Random lotto numbers

--- venv/Question 3.py
+++ venv/Question 3.py
@@ -1,11 +1,35 @@
  # This is Question 3 of Assignment 2
 # Francis Apurado
 #Lotto award checking system
+import random
 
 PWNs = []
 SWNs = []
 PlayerDetails = []
 PlayerNum = []
+
+def insertionSort(array, size):
+    for x in range(1, size):
+        key = array[x]
+        y = x - 1
+
+        # comparing the key array with each element on the left side until a smaller number is found
+        while y >= 0 and key < array[y]:
+            array[y + 1] = array[y]
+            y = y - 1
+
+        array[y + 1] = key
+
+rand = random.sample(range(1, 30), 8)
+
+
+for i in range(len(rand)):
+    x = rand[i]
+    if i <= 7 and i <=5 :
+        PWNs.append(x)
+    if i >=6 :
+        SWNs.append(x)
+
 
 while True:
 
@@ -18,6 +42,8 @@ while True:
 
         if Menu == 1:
             print("Showing data")
+            print("PWNs: ", PWNs)
+            print("SWNs: ", SWNs)
 
         if Menu == 2:
             print("Displaying statistics")
