eaabd93a6033c88285e844f30f51ae6313e18457
Heatclift0428 <apuradofrancis@gmail.com>
2022-05-02 19:06:05 +0800

    Added Display Statistics "Display How many lotto Winners based on Primary and Secondary numbers"

--- venv/Question 3.py
+++ venv/Question 3.py
@@ -2,11 +2,25 @@
 # Francis Apurado
 #Lotto award checking system
 import random
+import sys
+sys.setrecursionlimit(10000)
 
-PWNs = []
-SWNs = []
-PlayerDetails = []
-PlayerNum = []
+def BinarySearch(lys, val):
+    first = 0
+    last = len(lys)-1
+    index = -1
+    while (first <= last) and (index == -1):
+        mid = (first+last)//2
+        if lys[mid] == val:
+            index = mid
+        else:
+            if val<lys[mid]:
+                last = mid -1
+            else:
+                first = mid +1
+    return index
+
+
 
 def insertionSort(array, size):
     for x in range(1, size):
@@ -20,15 +34,114 @@ def insertionSort(array, size):
 
         array[y + 1] = key
 
-rand = random.sample(range(1, 30), 8)
+def swap(A, x, y):
+    tmp = A[x]
+    A[x] = A[y]
+    A[y] = tmp
+
+def SelectionSort(array, size):
+    for x in range(size):
+        minIndex = x
+
+        for y in range(x + 1, size):
+
+            if array[y] < array[minIndex]:  # will select the minimum for each loop #sorts in Ascending
+                minIndex = y
+        swap(array, minIndex, x)
 
 
-for i in range(len(rand)):
-    x = rand[i]
-    if i <= 7 and i <=5 :
-        PWNs.append(x)
-    if i >=6 :
-        SWNs.append(x)
+def mergeSort(array):
+    if len(array) > 1:
+
+        mid = len(array) // 2  # middle of the array
+        Left = array[:mid]
+        Right = array[mid:]
+
+        mergeSort(Left)  # sorting left side
+        mergeSort(Right)  # sorting right side
+
+        i = j = k = 0
+
+        # Temporary Arrays
+        while i < len(Left) and j < len(Right):
+            if Left[i] < Right[j]:
+                array[k] = Left[i]
+                i += 1
+            else:
+                array[k] = Right[j]
+                j += 1
+            k += 1
+
+        while i < len(Left):  # Checks if any elements in the Left array is left
+            array[k] = Left[i]
+            i += 1
+            k += 1
+
+        while j < len(Right):  # Checks if any elements in the Right array is left
+            array[k] = Right[j]
+            j += 1
+            k += 1
+
+# Pre-initilization phase
+
+PWNs = []
+SWNs = []
+PlayerNum = []
+count = 1
+
+for i in range(1000):
+    x = random.sample(range(1,30), 6)
+    mergeSort(x)
+    PlayerNum.append([[count], x])
+    count = count + 1
+
+randlotto = random.sample(range(1, 30), 8)
+PWNs = randlotto[0:6]
+SWNs = randlotto[6:8]
+
+insertionSort(PWNs, len(PWNs))
+SelectionSort(SWNs, len(SWNs))
+
+print(PlayerNum[0][1])
+print(PWNs)
+print(SWNs)
+
+First = 0
+Second = 0
+Third = 0
+Fourth = 0
+
+for x in range(1000):
+    P = 0
+    S = 0
+    print(PlayerNum[x][1])
+    for i in range(6):
+        result = BinarySearch(PlayerNum[x][1], PWNs[i])
+        if result != -1:
+            print("Primary Element is present")
+            P = P+1
+        else:
+            print("Primay Element is not present in array")
+
+    for y in range (2):
+        otherResult = BinarySearch(PlayerNum[x][1],SWNs[y])
+        if otherResult != -1:
+            print("Secondary Element is present")
+            S = S + 1
+        else:
+            print("Secondary Element is not present in array")
+
+    if P == 6:
+        First = First + 1
+
+    elif P == 5:
+        Second = Second + 1
+
+    elif P == 4:
+        Third = Third + 1
+
+    elif P == 3 & S == 2:
+        Fourth = Fourth + 1
 
 
 while True:
@@ -46,7 +159,13 @@ while True:
             print("SWNs: ", SWNs)
 
         if Menu == 2:
-            print("Displaying statistics")
+            print("Displaying statistics.....\n")
+            print("Winners Statistics Table:\n")
+            print("Winner Class   | Total Number of Winners\n")
+            print("1st Class:             ",First                  )
+            print("2nd Class:             ",Second)
+            print("3rd Class:            ", Third)
+            print("4th Class:            ", Fourth)
 
         if Menu == 3:
             print("Checking lotto status")
